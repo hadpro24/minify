ea6ba1f3f74776062968e488e047880799e38402
ourystd <amadoury313@gmail.com>
2021-11-18 02:35:41 +0000

    Bootstrap logout modal added

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/login.css";
-import "../styles/bootstrap.min.css";
 import "../styles/bootstrap-tweaks.css";
 import "../styles/prettify.css";
 import "../styles/default.css";
+import "../styles/globals.css";
 import "../styles/main.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
